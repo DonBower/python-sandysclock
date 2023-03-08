f206a1b720e3e976075e2247dc5d80f75afffe52
Don Bower <don.bower@outlook.com>
2023-03-08 14:16:42 -0800

    fix the size

--- dashboard.py
+++ dashboard.py
@@ -9,14 +9,16 @@ import PySimpleGUI as sg
 """
 
 
-theme_dict = {'BACKGROUND': '#2B475D',
-                'TEXT': '#FFFFFF',
-                'INPUT': '#F2EFE8',
-                'TEXT_INPUT': '#000000',
-                'SCROLL': '#F2EFE8',
-                'BUTTON': ('#000000', '#C2D4D8'),
-                'PROGRESS': ('#FFFFFF', '#C7D5E0'),
-                'BORDER': 1,'SLIDER_DEPTH': 0, 'PROGRESS_DEPTH': 0}
+theme_dict = {
+  'BACKGROUND': '#2B475D',
+  'TEXT': '#FFFFFF',
+  'INPUT': '#F2EFE8',
+  'TEXT_INPUT': '#000000',
+  'SCROLL': '#F2EFE8',
+  'BUTTON': ('#000000', '#C2D4D8'),
+  'PROGRESS': ('#FFFFFF', '#C7D5E0'),
+  'BORDER': 1,'SLIDER_DEPTH': 0, 'PROGRESS_DEPTH': 0
+}
 
 # sg.theme_add_new('Dashboard', theme_dict)     # if using 4.20.0.1+
 sg.LOOK_AND_FEEL_TABLE['Dashboard'] = theme_dict
@@ -26,7 +28,7 @@ BORDER_COLOR = '#C7D5E0'
 DARK_HEADER_COLOR = '#1B2838'
 BPAD_TOP = ((20,20), (20, 10))
 BPAD_LEFT = ((20,10), (0, 10))
-BPAD_LEFT_INSIDE = (0, 10)
+BPAD_LEFT_INSIDE = (0, 5)
 BPAD_RIGHT = ((10,20), (10, 20))
 
 top_banner = [
@@ -71,7 +73,18 @@ layout = [[sg.Column(top_banner, size=(960, 60), pad=(0,0), background_color=DAR
                       [sg.Column(block_3, size=(450,150),  pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT, background_color=BORDER_COLOR),
            sg.Column(block_4, size=(450, 320), pad=BPAD_RIGHT)]]
 
-window = sg.Window('Dashboard PySimpleGUI-Style', layout, margins=(0,0), background_color=BORDER_COLOR, no_titlebar=True, grab_anywhere=True)
+window = sg.Window(
+  'Dashboard PySimpleGUI-Style', 
+  layout, 
+  auto_size_text = True,
+  auto_size_buttons = True,
+  location = (0,0),
+  size = (800,480),
+  margins=(0,0), 
+  background_color=BORDER_COLOR, 
+  no_titlebar=True, 
+  grab_anywhere=True
+)
 
 while True:             # Event Loop
     event, values = window.read()
