6a78f83cf0e5f2c933c95c56e7452618884003a7
Don Bower <don.bower@outlook.com>
2023-03-10 13:17:06 -0800

    create layout

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import PySimpleGUI as sg
+from datetime import datetime
 
 """
     Dashboard using blocks of information.
@@ -31,16 +32,11 @@ BPAD_LEFT = ((20,10), (0, 10))
 BPAD_LEFT_INSIDE = (0, 5)
 BPAD_RIGHT = ((10,20), (10, 20))
 
-top_banner = [
+
+date_banner = [
   [
     sg.Text(
-      'Dashboard' + ' '*64, 
-      font='Any 20', 
-      text_color='blue', 
-      background_color=DARK_HEADER_COLOR
-    ),
-    sg.Text(
-      'Tuesday 9 June 2020', 
+      datetime.today().strftime('%A, %B %d %Y'), 
       font='Any 20', 
       text_color='yellow', 
       background_color=DARK_HEADER_COLOR
@@ -48,30 +44,139 @@ top_banner = [
   ]
 ]
 
-top  = [[sg.Text('The Weather Will Go Here', size=(50,1), justification='c', pad=BPAD_TOP, font='Any 20')],
-            [sg.T(f'{i*25}-{i*34}') for i in range(7)],]
+time_block = [
+  [
+    sg.Text(
+      '20:20', 
+      font='Digital'
+    )
+  ],
+]
 
-block_3 = [[sg.Text('Block 3', font='Any 20')],
-            [sg.Input(), sg.Text('Some Text')],
-            [sg.Button('Go'), sg.Button('Exit')]  ]
+am_block = [
+  [
+    sg.Text(
+      'AM',
+      font='Any 20'
+    )
+  ],
+]
+
+pm_block = [
+  [
+    sg.Text(
+      'AM',
+      font='Any 20'
+    )
+  ],
+]
+
+current_temp_block = [
+  [
+    sg.Text(
+      '103º',
+      font='Any 20'
+    )
+  ],
+]
+
+low_temp_block = [
+  [
+    sg.Text(
+      '101º',
+      font='Any 20'
+    )
+  ],
+]
+
+high_temp_block = [
+  [
+    sg.Text(
+      '109º',
+      font='Any 20'
+    )
+  ],
+]
+
+exit_block = [
+  [
+    sg.Button('Exit')
+  ]
+]
 
 
-block_2 = [[sg.Text('Block 2', font='Any 20')],
-            [sg.T('This is some random text')],
-            [sg.Image(data=sg.DEFAULT_BASE64_ICON)]  ]
 
-block_4 = [[sg.Text('Block 4', font='Any 20')],
-            [sg.T('This is some random text')],
-            [sg.T('This is some random text')],
-            [sg.T('This is some random text')],
-            [sg.T('This is some random text')]]
-
-
-layout = [[sg.Column(top_banner, size=(960, 60), pad=(0,0), background_color=DARK_HEADER_COLOR)],
-          [sg.Column(top, size=(920, 90), pad=BPAD_TOP)],
-          [sg.Column([[sg.Column(block_2, size=(450,150), pad=BPAD_LEFT_INSIDE)],
-                      [sg.Column(block_3, size=(450,150),  pad=BPAD_LEFT_INSIDE)]], pad=BPAD_LEFT, background_color=BORDER_COLOR),
-           sg.Column(block_4, size=(450, 320), pad=BPAD_RIGHT)]]
+layout = [
+  [
+    sg.Column(
+      date_banner, 
+      size=(700, 75), 
+      pad=(0,0), 
+      background_color=DARK_HEADER_COLOR
+    )
+  ],
+  [
+    sg.Column(
+      [
+        [
+          sg.Column(
+            time_block, 
+            size=(350,200), 
+            pad=BPAD_LEFT_INSIDE
+          )
+        ],
+        [
+          [
+            sg.Column(
+              am_block, 
+              size=(100,100),
+              pad=BPAD_LEFT_INSIDE
+            )
+          ],
+          [
+            sg.Column(
+              pm_block, 
+              size=(100,100),
+              pad=BPAD_LEFT_INSIDE
+            )
+          ]
+        ],
+        [
+          [
+            sg.Column(
+              current_temp_block, 
+              size=(250,100), 
+              pad=BPAD_LEFT_INSIDE
+            )
+          ],
+          [
+            [
+              sg.Column(
+                low_temp_block, 
+                size=(125,100),
+                pad=BPAD_LEFT_INSIDE
+              )
+            ],
+            [
+              sg.Column(
+                high_temp_block, 
+                size=(125,100),
+                pad=BPAD_LEFT_INSIDE
+              )
+            ]
+          ]
+        ]
+      ], 
+      pad=BPAD_LEFT, 
+      background_color=BORDER_COLOR
+    ),
+    sg.Column(
+      exit_block,
+      size=(50, 50),
+      pad=BPAD_RIGHT
+    )
+  ]
+]
 
 window = sg.Window(
   'Dashboard PySimpleGUI-Style', 
