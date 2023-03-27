3cf017845a75f73e56591537c762153921825945
Don Bower <don.bower@outlook.com>
2023-03-27 08:53:37 -0700

    ignore json

--- python-weatherkit.py
+++ python-weatherkit.py
@@ -8,9 +8,6 @@ key_id = os.environ.get('APPLE_KEY_ID')
 service_id = os.environ.get('APPLE_SERVICE_ID')
 private_key = os.environ.get('APPLE_PRIVATE_KEY')
 
-curl "https://weatherkit.apple.com/api/v1/weather/en/34.031392/-117.41704?dataSets=forecastDaily&countryCode=US&timezone=US/Los_Angeles" \
-  --header 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IkxCVjVXMjZaUkoiLCJpZCI6Ikw3NjYyQzdLWTYubmV0LmFnNmhxLnNhbmR5c2Nsb2NrIn0.eyJzdWIiOiJuZXQuYWc2aHEuc2FuZHlzY2xvY2siLCJpc3MiOiJMNzY2MkM3S1k2IiwiZXhwIjoxNjc5OTg2ODU3LCJpYXQiOjE2Nzk5MDA0NTcsImF1ZCI6Imh0dHBzOi8vd2VhdGhlcmtpdC5hcHBsZS5jb20vYXBpL3YxIn0.dzurOGHSE61Q2fipqn4wvFHSSLZc1l5geD1QDLO9WdFDiegUBO0lX2ehNYZNMTzyJjt3v-msibSrgbyJbVE8XA'
-
 # Instantiate the WeatherKit object
 wk_client = weatherkit.WeatherKit(team_id, service_id, private_key, key_id)
 
